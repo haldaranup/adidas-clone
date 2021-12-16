a2b2a7782620b31f4993e7229996a6870a789834
Anup Haldar <76952588+haldaranup@users.noreply.github.com>
2021-12-16 16:55:25 +0530

    Delete toggle.js

--- toggle.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".hamburger").addEventListener("click", myFunction);
-
-function myFunction(){
-    document.querySelector(".hamburger").classList.toggle("change")
-    
-    document.querySelector(".hamnames").classList.toggle("category")
-    
-}
