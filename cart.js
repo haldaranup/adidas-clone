3c87c8b351ff2bea5ed0e18da3335230c5b89e70
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2021-12-18 11:52:46 +0530

    update

---
+++ cart.js
@@ -0,0 +1,21 @@
+document.querySelector(".hamburger").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.querySelector(".hamburger").classList.toggle("change")
+
+    document.querySelector(".hamnames").classList.toggle("category")
+
+}
+
+function displaycart() {
+
+
+
+    var maindiv = document.createElement("div");
+    maindiv.setAttribute("id", "prod");
+    var div_img = document.createElement("div");
+    div_img.setAttribute("id", "fir");
+    var img = document.createElement("img");
+    img.setAttribute("scr",)
+}
+
